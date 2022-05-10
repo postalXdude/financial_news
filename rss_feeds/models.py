2f346f957820e929a7778a592b4527609084a476
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-05-11 01:10:15 +0200

    Initialize project and add rss_feeds app

---
+++ rss_feeds/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Symbols(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+
+class News(models.Model):
+    guid = models.CharField(max_length=50, primary_key=True)
+    title = models.TextField()
+    description = models.TextField()
+    link = models.TextField()
+    published_on = models.DateTimeField()
+    saved_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    symbol = models.ForeignKey(
+        Symbols,
+        on_delete=models.CASCADE,
+        related_name='News'
+    )
